96185ef30d7653bd0224ae4ad4882103d8f20028
Renee Gil <renee.gil@mailbox.org>
2026-05-11 20:21:33 -0400

    update

---
+++ src/site.config.ts
@@ -0,0 +1,55 @@
+import type { AstroExpressiveCodeOptions } from "astro-expressive-code";
+import type { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+	url: "https://rngil.github.io/",
+	title: "Renee Gil",
+	author: "Renee Gil",
+	description: "Applied mathematician & HPC engineer. FOSS contributor and independent researcher.",
+	lang: "en",
+	ogLocale: "en_US",
+	date: {
+		locale: "en-US",
+		options: {
+			day: "numeric",
+			month: "short",
+			year: "numeric",
+		},
+	},
+};
+
+export const menuLinks: { path: string; title: string }[] = [
+	{ path: "/", title: "About" },
+	{ path: "/oss/", title: "Open Source" },
+	{ path: "/research/", title: "Research & Outreach" },
+];
+
+// https://expressive-code.com/reference/configuration/
+export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
+	styleOverrides: {
+		borderRadius: "4px",
+		codeFontFamily:
+			'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+		codeFontSize: "0.875rem",
+		codeLineHeight: "1.7142857rem",
+		codePaddingInline: "1rem",
+		frames: {
+			frameBoxShadowCssValue: "none",
+		},
+		uiLineHeight: "inherit",
+	},
+	themeCssSelector(theme, { styleVariants }) {
+		// If one dark and one light theme are available
+		// generate theme CSS selectors compatible with cactus-theme dark mode switch
+		if (styleVariants.length >= 2) {
+			const baseTheme = styleVariants[0]?.theme;
+			const altTheme = styleVariants.find((v) => v.theme.type !== baseTheme?.type)?.theme;
+			if (theme === baseTheme || theme === altTheme) return `[data-theme='${theme.type}']`;
+		}
+		// return default selector
+		return `[data-theme="${theme.name}"]`;
+	},
+	// One dark, one light theme => https://expressive-code.com/guides/themes/#available-themes
+	themes: ["dracula", "github-light"],
+	useThemedScrollbars: false,
+};
